e977a4748bfa90592aeccebff13c6adcf5c9b66f
Benjamin Chang (OSV) <benjamin.chang.osv@fedex.com>
2021-04-06 16:36:24 -0400

    install material UI, disable ivy; material is still buggy

--- src/app/test/test.component.spec.ts
+++ src/app/test/test.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatMenuModule } from "@angular/material";
 
 import { TestComponent } from './test.component';
 
--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TestComponent } from './test.component';
 import {Router, RouterModule, Routes} from '@angular/router';
+import { MatMenuModule } from "@angular/material";
+import { MatIconModule } from "@angular/material";
 
 @NgModule({
   declarations: [TestComponent],
   imports: [
     CommonModule,
     RouterModule,
+    MatMenuModule,
+    MatIconModule
   ],
   entryComponents: [
     TestComponent,
